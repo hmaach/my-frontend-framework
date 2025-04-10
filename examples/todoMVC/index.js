77bc3321507cc7ee1760267544c3d638af8fb968
hkhlifi <khllifihamza66@gmail.com>
2025-04-10 10:16:51 +0100

    v0.2

--- examples/todoMVC/index.js
+++ examples/todoMVC/index.js
@@ -2,9 +2,9 @@ import { createApp, HashRouter } from "../../src/index.js";
 import { App } from "./src/app.js";
 
 const router = new HashRouter([
-    { path: "/", component: null },
-    { path: "/active", component: null },
-    { path: "/completed", component: null }
+    { path: "/"},
+    { path: "/active"},
+    { path: "/completed"}
 ]);
 
 const app = createApp(App, {}, { router });
